592636be2737081f94bf9a8c939489573ec2bb1d
Magnus Zetterberg <magnus.zetterberg@gmail.com>
2024-07-14 18:08:02 +0200

    another fix

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ echo "** Updating done **"
 echo "*******************"
 echo ""
 echo "*** Installing needed packages... ***"
-sudo pacman -S hyprland git waybar alacritty chromium rofi vscodium nerd-fonts python-requests
+sudo pacman -Sy hyprland git waybar alacritty chromium rofi code nerd-fonts python-requests
 
 # echo ""
 # echo ""
