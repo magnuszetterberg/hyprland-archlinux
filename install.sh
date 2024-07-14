3d7cb83bba071fa1ebca0b0303482b373320480c
Magnus Zetterberg <magnus.zetterberg@gmail.com>
2024-07-14 17:00:44 +0200

    adding an install.sh

---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#Setup base-packages
+echo "*** updating system ***"
+echo ""
+sudo pacman -Suy
+echo ""
+echo "** Updating done **"
+echo "*******************"
+echo ""
+echo "*** Installing needed packages... ***"
+sudo pacman -S hyprland \
+    git \
+    waybar \
+    alacritty \
+    chromium \
+    rofi \
+    vscodium \
+    nerd-fonts
+
+# echo ""
+# echo ""
+# echo "*** Installing nerdfont ***"
+# #Install nerd-fonts for icons
+# sudo yay -S nerd-fonts
+
+
+# clone the config to .
+echo ""
+echo "*** cloning git repo ***"
+echo ""
+git clone https://github.com/magnuszetterberg/hyprland-archlinux.git
+echo ""
+echo "************************"
+
+cd hyprland-archlinux
+echo ""
+echo "*** Copying config to .config folder ***"
+sleep 3
+echo ""
+cp -r ./waybar/ ~/.config/
+sleep 3
+echo ""
+cp -r ./hypr/ ~/.config/
+echo ""
+echo "*** All done ***"
